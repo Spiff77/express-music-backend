6d59b6e9ead659e317409420e1b754286a21bf7c
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-08-23 11:35:02 +0800

    modifying stuffs

--- app/app.js
+++ app/app.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json()); // More modern approach than using the body-parser library. it is now not required.
 
 app.use('/artists', artistRoutes);
-
 app.listen(8081, () => {
     console.log(`Server is running on port 8081`);
 });
