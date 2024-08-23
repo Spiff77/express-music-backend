436bb847652d0891b31a765fce3de16e6e494cca
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-08-23 10:50:07 +0800

    backend

---
+++ app/config/db.js
@@ -0,0 +1,11 @@
+// config/db.js
+import mysql from 'mysql2/promise';
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'music_db',
+});
+
+export default connection;
